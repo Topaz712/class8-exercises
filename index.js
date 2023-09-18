894e13802a3a7361087ac80da8e7117e517851e0
Topaz712 <tzam712@gmail.com>
2023-09-18 13:48:13 -0500

    finished exercise one
    started on two

---
+++ index.js
@@ -0,0 +1,10 @@
+// exercise one
+//Use JavaScript to select the element with an ID of "myDiv" and change its background color to "lightblue". Select all the paragraph (<p>) elements on the page and log their inner text.
+const myDiv = document.getElementById('myDiv');
+myDiv.style.backgroundColor = 'lightblue';
+
+const paragraphs = document.querySelectorAll('p');
+paragraphs.forEach(paragraph => {
+  console.log(paragraph.innerText)});
+
+
